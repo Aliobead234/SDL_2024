d8c1b64c8358133bf3ac3af6f2422b08c9d9e95b
Aliobead234 <157116218+Aliobead234@users.noreply.github.com>
2024-01-25 14:12:21 +0300

    uploading file "homeworks"
    file "homeworks" will contain the answers of the problems/hws for SDL program module 3/4.

---
+++ Homeworks/1/dotnet/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Npgsql;
+
+Console.WriteLine("Hello, World!");
+
+using Npgsql;
+
+var connectionString = "Host=localhost;Username=Ali;Password=190668;Database=sdl_ali";
+
+await using var dataSource = NpgsqlDataSource.Create(connectionString);
+
+// Retrieve all rows
+await using (var cmd = dataSource.CreateCommand("SELECT VERSION();"))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+    {
+        Console.WriteLine(reader.GetString(0));
+    }
+}
