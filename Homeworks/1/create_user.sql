bcc29f00f7a8e42d7164b045b19f864e0c1d2567
Aliobead234 <157116218+Aliobead234@users.noreply.github.com>
2024-01-26 22:47:27 +0000

    Uploading "Go" file

--- Homeworks/1/create_user.sql
+++ Homeworks/1/create_user.sql
@@ -1,4 +1,4 @@
 CREATE DATABASE sdl_ali;
-
+-- Перейти на БД sdl_ali; / or creating a new database in the name of sdl_ali
 CREATE ROLE "Ali" LOGIN PASSWORD '190668';
 GRANT USAGE ON SCHEMA public TO "Ali";
